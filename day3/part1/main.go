064f962fd3524a624957ac4d0d3b3ec7084456cb
Joe M <joseph.mcgrath@aviva.com>
2023-12-06 15:57:10 +0000

    Complete day 3 part 1

--- day3/part1/main.go
+++ day3/part1/main.go
@@ -9,34 +9,36 @@ import (
 	"unicode"
 )
 
-type number struct {
+type Number struct {
 	value        int
 	yCoordinate  int
 	xCoordinates []int
 }
 
-type symbol struct {
+type Symbol struct {
 	xCoordinate int
 	yCoordinate int
 }
 
-func main() {
-	file, err := os.Open("./day3/input.txt")
+func OpenFile(filepath string) *os.File {
+	file, err := os.Open(filepath)
 	aoc.Check(err)
-	defer func() {
-		err := file.Close()
-		aoc.Check(err)
-	}()
+	return file
+}
 
-	scanner := bufio.NewScanner(file)
-	symbols := make(map[symbol]bool)
-	var numbers []number
+func ParseSymbolsAndNumbers(scanner *bufio.Scanner) (map[Symbol]bool, []Number) {
+	symbols := make(map[Symbol]bool)
+	var numbers []Number
 
 	for lineNumber := 0; scanner.Scan(); lineNumber++ {
 		numbersOnLine := parseNumbersAndSymbolsOnLine(lineNumber, scanner.Text(), symbols)
 		numbers = append(numbers, numbersOnLine...)
 	}
 
+	return symbols, numbers
+}
+
+func CalculateSum(symbols map[Symbol]bool, numbers []Number) int {
 	sum := 0
 	for _, num := range numbers {
 		if isAdjacentToSymbol(num, symbols) {
@@ -44,34 +46,50 @@ func main() {
 		}
 	}
 
+	return sum
+}
+
+func main() {
+	file := OpenFile("./day3/input.txt")
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	symbols, numbers := ParseSymbolsAndNumbers(scanner)
+
+	sum := CalculateSum(symbols, numbers)
+
 	fmt.Println("Sum of all part numbers:", sum)
 }
 
-func parseNumbersAndSymbolsOnLine(lineNumber int, line string, symbols map[symbol]bool) (numbersOnLine []number) {
+func parseNumbersAndSymbolsOnLine(lineNumber int, line string, symbols map[Symbol]bool) []Number {
+	var numbersOnLine []Number
 	currentNumber := ""
+
 	for x, char := range line {
 		if unicode.IsDigit(char) {
 			currentNumber += string(char)
 		} else {
 			if currentNumber != "" {
 				num, _ := strconv.Atoi(currentNumber)
-				numbersOnLine = append(numbersOnLine, number{
+				numbersOnLine = append(numbersOnLine, Number{
 					value:        num,
 					yCoordinate:  lineNumber,
 					xCoordinates: []int{x - len(currentNumber), x - 1},
 				})
 				currentNumber = ""
 			}
+
 			if char != '.' {
-				symbols[symbol{xCoordinate: x, yCoordinate: lineNumber}] = true
+				symbols[Symbol{xCoordinate: x, yCoordinate: lineNumber}] = true
 			}
 		}
 	}
+
 	if currentNumber != "" {
 		num, err := strconv.Atoi(currentNumber)
 		aoc.Check(err)
 		startCoordinate := len(line) - len(currentNumber)
-		numbersOnLine = append(numbersOnLine, number{
+		numbersOnLine = append(numbersOnLine, Number{
 			value:        num,
 			yCoordinate:  lineNumber,
 			xCoordinates: []int{startCoordinate, len(line) - 1},
@@ -81,15 +99,16 @@ func parseNumbersAndSymbolsOnLine(lineNumber int, line string, symbols map[symbo
 	return numbersOnLine
 }
 
-func isAdjacentToSymbol(num number, symbols map[symbol]bool) bool {
+func isAdjacentToSymbol(num Number, symbols map[Symbol]bool) bool {
 	for _, x := range num.xCoordinates {
 		for dx := -1; dx <= 1; dx++ {
 			for dy := -1; dy <= 1; dy++ {
-				if _, exists := symbols[symbol{xCoordinate: x + dx, yCoordinate: num.yCoordinate + dy}]; exists {
+				if _, exists := symbols[Symbol{xCoordinate: x + dx, yCoordinate: num.yCoordinate + dy}]; exists {
 					return true
 				}
 			}
 		}
 	}
+
 	return false
 }
