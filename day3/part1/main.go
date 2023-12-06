d9cf66f5a5cea8d922b98fb126df9ebaddecff8b
Joe M <joseph.mcgrath@aviva.com>
2023-12-06 15:56:43 +0000

    Complete day 3 part 1

--- day3/part1/main.go
+++ day3/part1/main.go
@@ -2,38 +2,94 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	aoc "github.com/jdmcgrath/AoC-2023"
 	"os"
+	"strconv"
 	"unicode"
 )
 
 type number struct {
-	coordinateX, coordinateY, number int
+	value        int
+	yCoordinate  int
+	xCoordinates []int
+}
+
+type symbol struct {
+	xCoordinate int
+	yCoordinate int
 }
 
 func main() {
 	file, err := os.Open("./day3/input.txt")
 	aoc.Check(err)
 	defer func() {
-		aoc.Check(file.Close())
+		err := file.Close()
+		aoc.Check(err)
 	}()
-	var fileOfChars []string
-	scanner := bufio.NewScanner(file)
-	for lineNumber := 0; scanner.Scan(); lineNumber++ {
-		fileOfChars = append(fileOfChars, scanner.Text())
-	}
-}
 
-func parseNumbersAndCoordinatesOnLine(line []rune) []number {
-	inNumber := false
-	var number []rune
-	for xCoordinate, character := range line {
-		if isDigit(character) {
-			number = append(number, character)
+	scanner := bufio.NewScanner(file)
+	symbols := make(map[symbol]bool)
+	var numbers []number
+
+	for lineNumber := 0; scanner.Scan(); lineNumber++ {
+		numbersOnLine := parseNumbersAndSymbolsOnLine(lineNumber, scanner.Text(), symbols)
+		numbers = append(numbers, numbersOnLine...)
+	}
+
+	sum := 0
+	for _, num := range numbers {
+		if isAdjacentToSymbol(num, symbols) {
+			sum += num.value
 		}
 	}
+
+	fmt.Println("Sum of all part numbers:", sum)
 }
 
-func isDigit(character rune) bool {
-	return unicode.IsDigit(character)
+func parseNumbersAndSymbolsOnLine(lineNumber int, line string, symbols map[symbol]bool) (numbersOnLine []number) {
+	currentNumber := ""
+	for x, char := range line {
+		if unicode.IsDigit(char) {
+			currentNumber += string(char)
+		} else {
+			if currentNumber != "" {
+				num, _ := strconv.Atoi(currentNumber)
+				numbersOnLine = append(numbersOnLine, number{
+					value:        num,
+					yCoordinate:  lineNumber,
+					xCoordinates: []int{x - len(currentNumber), x - 1},
+				})
+				currentNumber = ""
+			}
+			if char != '.' {
+				symbols[symbol{xCoordinate: x, yCoordinate: lineNumber}] = true
+			}
+		}
+	}
+	if currentNumber != "" {
+		num, err := strconv.Atoi(currentNumber)
+		aoc.Check(err)
+		startCoordinate := len(line) - len(currentNumber)
+		numbersOnLine = append(numbersOnLine, number{
+			value:        num,
+			yCoordinate:  lineNumber,
+			xCoordinates: []int{startCoordinate, len(line) - 1},
+		})
+	}
+
+	return numbersOnLine
+}
+
+func isAdjacentToSymbol(num number, symbols map[symbol]bool) bool {
+	for _, x := range num.xCoordinates {
+		for dx := -1; dx <= 1; dx++ {
+			for dy := -1; dy <= 1; dy++ {
+				if _, exists := symbols[symbol{xCoordinate: x + dx, yCoordinate: num.yCoordinate + dy}]; exists {
+					return true
+				}
+			}
+		}
+	}
+	return false
 }
